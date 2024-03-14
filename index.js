aea43807f077dbec4afeb0e7fb67a02a343ce830
Alexiszxc22 <Alexiszxc22>
2024-03-14 14:26:58 +0800

    added functions to the letters N,G,O

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Hello World!");
 
-generateNumber(30);
+generateNumber(75);
 
 function generateNumber(max) {
    // Add = +
@@ -16,6 +16,14 @@ function generateNumber(max) {
       console.log('The number is in B.')
    } else if (randomNumber >= 16 && randomNumber <= 30) {
       console.log('The number is in I.')
+   } else if (randomNumber >= 31 && randomNumber <= 45) {
+      console.log('The number is in N.')
+   }
+   else if (randomNumber >= 46 && randomNumber <= 55) {
+      console.log('The number is in G.')
+   }
+   else if (randomNumber >= 56 && randomNumber <= 75) {
+      console.log('The number is in O')
    }
    else {
       console.log('The number is invalid.')
