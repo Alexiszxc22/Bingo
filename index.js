3e279dc0e24c8665f89254ae190d92f88389347e
Alexiszxc22 <Alexiszxc22>
2024-03-14 14:18:25 +0800

    added new bingo functions

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("Hello World!");
+
+generateNumber(30);
+
+function generateNumber(max) {
+   // Add = +
+   // Sub = -
+   // Div = /
+   // Mult = *
+   // Rem = %
+   let randomNumber = Math.floor(Math.random() * max) + 1;
+
+   // if else if else
+   // GT>, GTE >=, LT<, LTE <=, EQ =, NEQ !=, AND && , OR||
+   if (randomNumber <= 15) {
+      console.log('The number is in B.')
+   } else if (randomNumber >= 16 && randomNumber <= 30) {
+      console.log('The number is in I.')
+   }
+   else {
+      console.log('The number is invalid.')
+   }
+
+   return console.log(randomNumber);
+}
